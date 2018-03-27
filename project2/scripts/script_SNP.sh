c24b24bb15b7faa2552668dab154fa9afbe1d03f
Jason Miller <jasonmiller@Jasons-MacBook-Air.local>
2018-03-27 18:22:11 -0400

    Fix bug of space inside quotes.

--- project2/scripts/script_SNP.sh
+++ project2/scripts/script_SNP.sh
@@ -4,14 +4,14 @@ echo "#!/bin/sh"
 
 while read line; do
     set -- $line
-    echo "echo    \"" $1,$2,$3,$4,$5,$6, "fwd,R1\""
-    echo "grep -c \"" $6 "\" Col_x_Ler_BR?_R1.seq"
-    echo "echo    \"" $1,$2,$3,$4,$5,$6, "fwd,R2\""
-    echo "grep -c \"" $6 "\" Col_x_Ler_BR?_R2.seq"
-    echo "echo    \"" $1,$2,$3,$4,$5,$7, "rev,R1\""
-    echo "grep -c \"" $7 "\" Col_x_Ler_BR?_R1.seq"
-    echo "echo    \"" $1,$2,$3,$4,$5,$7, "rev,R2\""
-    echo "grep -c \"" $7 "\" Col_x_Ler_BR?_R2.seq"
+    echo "echo    \""$1,$2,$3,$4,$5,$6,"fwd,R1\""
+    echo "grep -c \""$6"\" Col_x_Ler_BR?_R1.seq"
+    echo "echo    \""$1,$2,$3,$4,$5,$6,"fwd,R2\""
+    echo "grep -c \""$6"\" Col_x_Ler_BR?_R2.seq"
+    echo "echo    \""$1,$2,$3,$4,$5,$7,"rev,R1\""
+    echo "grep -c \""$7"\" Col_x_Ler_BR?_R1.seq"
+    echo "echo    \""$1,$2,$3,$4,$5,$7,"rev,R2\""
+    echo "grep -c \""$7"\" Col_x_Ler_BR?_R2.seq"
     echo ""
 done
 
